b6280b3cc37f87fdd35cdf0696bc973e11d65695
Idan Attias <idanzii@gmail.com>
2018-12-26 07:50:27 +0200

    support nil in prints to repl

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function repl() {
       const ast = parse(code)
       const result = vm.runLine(ast.first())
       if (result !== undefined) {
-        console.log(';', result.toString())
+        console.log(';', result === null ? 'nil' : result.toString())
       }
     } catch(e) {
       console.error(e)
