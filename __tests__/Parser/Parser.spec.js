b1154fc9b29f3b3f47f08530a52cc97fc623bd89
Idan Attias <idanzii@gmail.com>
2019-01-04 19:48:38 +0200

    introduce parser test matchers

--- __tests__/Parser/Parser.spec.js
+++ __tests__/Parser/Parser.spec.js
@@ -22,41 +22,27 @@ const { ParserError, SyntaxError } = require('../../lib/errors')
 describe('Parser', () => {
   describe('let statement', () => {
     it('parses let statement correctly', () => {
-      const tokens = withTokensFrom('let x 1')
-
-      expect(parse(tokens)).toEqual(new AST([
-        new LetStatement(
-          new IntLiteral(1),
-          'x'
-        )
-      ]))
+      expect('let x 1').toParseTo([new LetStatement(new IntLiteral(1), 'x')])
     })
 
     it('throws ParseError when second token is not an identifier', () => {
-      const tokens = withTokensFrom('let let 1')
-
-      expect(() => parse(tokens))
-        .toThrowWithMessage(ParserError, "expected 'let' to be an identifier (1:5)")
+      expect('let let 1').toFailParsing("expected 'let' to be an identifier (1:5)")
     })
   })
 
   describe('if statement', () => {
     it('parses simple if statement correctly', () => {
-      const tokens = withTokensFrom('if (= x 1) { 42 }')
-
-      expect(parse(tokens)).toEqual(new AST([
+      expect('if (= x 1) { 42 }').toParseTo([
         new IfStatement(
           new FunctionCall(new IdentifierExpression('='), [new IdentifierExpression('x'), new IntLiteral(1)]),
           [new IntLiteral(42)],
           null
         )
-      ]))
+      ])
     })
 
     it('parses an if-else statement correctly', () => {
-      const tokens = withTokensFrom('if (= x 1) { 42 } else { 45 }')
-
-      expect(parse(tokens)).toEqual(new AST([
+      expect('if (= x 1) { 42 } else { 45 }').toParseTo([
         new IfStatement(
           new FunctionCall(new IdentifierExpression('='), [new IdentifierExpression('x'), new IntLiteral(1)]),
           [new IntLiteral(42)],
@@ -65,13 +51,11 @@ describe('Parser', () => {
             null
           )
         )
-      ]))
+      ])
     })
 
     it('parses an if-elsif statement correctly', () => {
-      const tokens = withTokensFrom('if (= x 1) { 42 } elsif (= x 2) { 45 }')
-
-      expect(parse(tokens)).toEqual(new AST([
+      expect('if (= x 1) { 42 } elsif (= x 2) { 45 }').toParseTo([
         new IfStatement(
           new FunctionCall(new IdentifierExpression('='), [new IdentifierExpression('x'), new IntLiteral(1)]),
           [new IntLiteral(42)],
@@ -81,13 +65,11 @@ describe('Parser', () => {
             null
           )
         )
-      ]))
+      ])
     })
 
     it('parses an if-elsif-else statement correctly', () => {
-      const tokens = withTokensFrom('if (= x 1) { 42 } elsif (= x 2) { 43 } elsif (= x 3) { 44 } else { 45 }')
-
-      expect(parse(tokens)).toEqual(new AST([
+      expect('if (= x 1) { 42 } elsif (= x 2) { 43 } elsif (= x 3) { 44 } else { 45 }').toParseTo([
         new IfStatement(
           new FunctionCall(new IdentifierExpression('='), [new IdentifierExpression('x'), new IntLiteral(1)]),
           [new IntLiteral(42)],
@@ -104,147 +86,101 @@ describe('Parser', () => {
             )
           )
         )
-      ]))
+      ])
     })
 
     it('throws when if body is not inside a scope ({...})', () => {
-      const tokens = withTokensFrom('if (= x 1) 42')
-
-      expect(() => parse(tokens)).toThrowWithMessage(ParserError, "expected '42' to be '{' (1:12)")
+      expect('if (= x 1) 42').toFailParsing("expected '42' to be '{' (1:12)")
     })
 
     it('throws when elsif body is not inside a scope ({...})', () => {
-      const tokens = withTokensFrom('if (= x 1) { 42 } elsif (= x 4) 43')
-
-      expect(() => parse(tokens)).toThrowWithMessage(ParserError, "expected '43' to be '{' (1:33)")
+      expect('if (= x 1) { 42 } elsif (= x 4) 43').toFailParsing("expected '43' to be '{' (1:33)")
     })
 
     it('throws when els body is not inside a scope ({...})', () => {
-      const tokens = withTokensFrom('if (= x 1) { 42 } else 43')
-
-      expect(() => parse(tokens)).toThrowWithMessage(ParserError, "expected '43' to be '{' (1:24)")
+      expect('if (= x 1) { 42 } else 43').toFailParsing("expected '43' to be '{' (1:24)")
     })
   })
 
   describe('expressions', () => {
     describe('function call', () => {
       it('parses a simple function call correctly', () => {
-        const tokens = withTokensFrom('(f)')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new FunctionCall(new IdentifierExpression('f'), [])
-        ]))
+        expect('(f)').toParseTo([new FunctionCall(new IdentifierExpression('f'), [])])
       })
 
       it('parses a simple function call with arguments correctly', () => {
-        const tokens = withTokensFrom('(f x 2)')
-  
-        expect(parse(tokens)).toEqual(new AST([
+        expect('(f x 2)').toParseTo([
           new FunctionCall(
             new IdentifierExpression('f'),
             [new IdentifierExpression('x'), new IntLiteral(2)]
           )
-        ]))
+        ])
       })
 
       it('parses a dynamic function call correctly', () => {
-        const tokens = withTokensFrom('((f) 1 2 3)')
-
-        expect(parse(tokens)).toEqual(new AST([
+        expect('((f) 1 2 3)').toParseTo([
           new FunctionCall(
             new FunctionCall(new IdentifierExpression('f'), []),
             [new IntLiteral(1), new IntLiteral(2), new IntLiteral(3)]
           )
-        ]))
+        ])
       })
 
       it('throws a ParserError when function call does not end with a closing parenthesis', () => {
-        const tokens = withTokensFrom('(f 1')
-
-        expect(() => parse(tokens)).toThrowWithMessage(ParserError, "expected '' to be ')' (1:5)")
+        expect('(f 1').toFailParsing("expected '' to be ')' (1:5)")
       })
     })
 
     describe('literals', () => {
       it('parses integer correctly', () => {
-        const tokens = withTokensFrom('1')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new IntLiteral(1)
-        ]))
+        expect('1').toParseTo([new IntLiteral(1)])
       })
 
       it('parses float correctly', () => {
-        const tokens = withTokensFrom('-1.5')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new FloatLiteral(-1.5)
-        ]))
+        expect('-1.5').toParseTo([new FloatLiteral(-1.5)])
       })
 
       it('parses string correctly', () => {
-        const tokens = withTokensFrom('"Hi"')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new StringLiteral('"Hi"')
-        ]))
+        expect('"Hi"').toParseTo([new StringLiteral('"Hi"')])
       })
 
       it('parses true correctly', () => {
-        const tokens = withTokensFrom('true')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new TrueLiteral()
-        ]))
+        expect('true').toParseTo([new TrueLiteral()])
       })
 
       it('parses false correctly', () => {
-        const tokens = withTokensFrom('false')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new FalseLiteral()
-        ]))
+        expect('false').toParseTo([new FalseLiteral()])
       })
 
       it('parses nil correctly', () => {
-        const tokens = withTokensFrom('nil')
-
-        expect(parse(tokens)).toEqual(new AST([
-          new NilLiteral()
-        ]))
+        expect('nil').toParseTo([new NilLiteral()])
       })
 
       describe('function expression', () => {
         it('parses a one line function correctly', () => {
-          const tokens = withTokensFrom('[] 42')
-          
-          expect(parse(tokens)).toEqual(new AST([
+          expect('[] 42').toParseTo([
             new FunctionExpression(
               'anonymous',
               [new ReturnStatement(new IntLiteral(42))],
               [],
               false
             )
-          ]))
+          ])
         })
 
         it('parses a multi line function correctly', () => {
-          const tokens = withTokensFrom('[] { return 42 }')
-
-          expect(parse(tokens)).toEqual(new AST([
+          expect('[] { return 42 }').toParseTo([
             new FunctionExpression(
               'anonymous',
               [new ReturnStatement(new IntLiteral(42))],
               [],
               false
             )
-          ]))
+          ])
         })
 
         it('parses a named function correctly', () => {
-          const tokens = withTokensFrom('let func [] 42')
-
-          expect(parse(tokens)).toEqual(new AST([
+          expect('let func [] 42').toParseTo([
             new LetStatement(
               new FunctionExpression(
                 'func',
@@ -254,68 +190,44 @@ describe('Parser', () => {
               ),
               'func'
             )
-          ]))
+          ])
         })
 
         it('parses function arguments correctly', () => {
-          const tokens = withTokensFrom('[a b @c] 42')
-
-          expect(parse(tokens)).toEqual(new AST([
+          expect('[a b @c] 42').toParseTo([
             new FunctionExpression(
               'anonymous',
               [new ReturnStatement(new IntLiteral(42))],
               ['a', 'b', '@c'],
               true
             )
-          ]))
+          ])
         })
 
         it('throws when function arguments list is not closed', () => {
-          const tokens = withTokensFrom('[')
-
-          expect(() => parse(tokens)).toThrowWithMessage(ParserError, "expected '' to be ']' (1:2)")
+          expect('[').toFailParsing("expected '' to be ']' (1:2)")
         })
 
         it('throws when the same argument name is used in two different args', () => {
-          const tokens = withTokensFrom('[a a] 42')
-
-          expect(() => parse(tokens)).toThrowWithMessage(ParserError, "double argument error: 'a' already exists (1:4)")
+          expect('[a a] 42').toFailParsing("double argument error: 'a' already exists (1:4)")
         })
 
         it('throws when the same argument name is used in an argument and varargs', () => {
-          const tokens = withTokensFrom('[a @a] 42')
-
-          expect(() => parse(tokens)).toThrowWithMessage(ParserError, "double argument error: '@a' already exists (1:4)")
+          expect('[a @a] 42').toFailParsing("double argument error: '@a' already exists (1:4)")
         })
 
         it('throws when varargs is used as the non last parameter', () => {
-          const tokens = withTokensFrom('[@a b] 42')
-
-          expect(() => parse(tokens)).toThrowWithMessage(ParserError, "'@a' must be used as the last argument (1:2)")
+          expect('[@a b] 42').toFailParsing("'@a' must be used as the last argument (1:2)")
         })
 
         it('throws when given argument is not an identifier', () => {
-          const tokens = withTokensFrom('[4] 42')
-
-          expect(() => parse(tokens)).toThrowWithMessage(ParserError, "expected '4' to be an identifier (1:2)")
+          expect('[4] 42').toFailParsing("expected '4' to be an identifier (1:2)")
         })
       })
 
       it('throws syntax error when expression is invalid', () => {
-        const tokens = withTokensFrom('{')
-
-        expect(() => parse(tokens)).toThrowWithMessage(SyntaxError, "invalid syntax '{' (1:1)")
+        expect('{').toThrowSyntaxErrorInParsing({suffix: "'{'", row: 1, col: 1})
       })
     })
   })
 })
-
-function withTokensFrom(code) {
-  return createLexer().tokenize(code)
-}
-
-function parse(tokens) {
-  const parser = new Parser()
-
-  return parser.parse(tokens)
-}
--- __tests__/matchers.js
+++ __tests__/matchers.js
@@ -1,23 +1,51 @@
 const createLexer = require('../lib/Lexer')
 const { EOF } = require('../lib/Lexer/symbols')
 const Token = require('../lib/Lexer/Token')
-const { InvalidTokenError } = require('../lib/errors')
+const { InvalidTokenError, SyntaxError, ParserError } = require('../lib/errors')
+const Parser = require('../lib/Parser')
+const { AST } = require('../lib/AST')
 
 require('jest-extended')
 
 expect.extend({
   toTokenizeTo(code, list) {
-    const lexer = createLexer()
     const lastToken = new Token(EOF, '', expect.any(Number), expect.any(Number))
-    expect(lexer.tokenize(code)).toEqual([...list, lastToken])
+    expect(tokenize(code)).toEqual([...list, lastToken])
 
     return {pass: true}
   },
 
   toFailTokenizing(code, errorMessage) {
-    const lexer = createLexer()
-    expect(() => lexer.tokenize(code)).toThrowWithMessage(InvalidTokenError, errorMessage)
+    expect(() => tokenize(code)).toThrowWithMessage(InvalidTokenError, errorMessage)
+
+    return {pass: true}
+  },
+
+  toParseTo(code, astBody) {
+    expect(parse(code)).toEqual(new AST(astBody))
+
+    return {pass: true}
+  },
+
+  toThrowSyntaxErrorInParsing(code, {suffix, row, col}) {
+    expect(() => parse(code)).toThrowWithMessage(SyntaxError, `invalid syntax ${suffix} (${row}:${col})`)
+
+    return {pass: true}
+  },
+
+  toFailParsing(code, errorMessage) {
+    expect(() => parse(code)).toThrowWithMessage(ParserError, errorMessage)
 
     return {pass: true}
   }
 })
+
+function tokenize(code) {
+  const lexer = createLexer()
+  return lexer.tokenize(code)
+}
+
+function parse(code) {
+  const tokens = tokenize(code)
+  return new Parser().parse(tokens)
+}
